855522b7cc9f7359043a2452ac605a56e479c4d4
moth2000 <1581257986@qq.com>
2022-07-22 16:15:35 +0800

    java基础语法训练day4

--- src/main/java/Function_ex.java
+++ src/main/java/Function_ex.java
@@ -1,3 +1,18 @@
-public class Function_ex {
+import java.security.PublicKey;
 
+public class Function_ex {
+    public static void main(String[] args) {
+        //运行逢七过方法
+        doGmae();
+    }
+    //创建逢七过方法
+    public static void doGmae(){
+        //使用for循环获得1到20的数字
+        for (int i = 1;i <= 20;i++){
+            //判断数字是否是7的倍数或者含数字7
+            if(i % 7 == 0 || i % 10 == 7 || i / 10 == 7){
+                System.out.println("数字" + i + "过!");
+            }
+        }
+    }
 }
